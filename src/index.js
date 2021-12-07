2e0c702afd0f35be75a0d6f44d18863aa8842aa3
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-12-07 18:05:08 +0100

    Transform index.js into ts file

--- src/index.js
+++
@@ -1,6 +0,0 @@
-exports.XML = require('./src/xml');
-exports.Apex = require('./src/apex');
-exports.Aura = require('./src/aura');
-exports.JavaScript = require('./src/javascript');
-exports.System = require('./src/system');
-exports.LanguageUtils = require('./src/utils').LanguageUtils;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * as XML from './xml';
+export * as Apex from './apex';
+export * as Aura from './aura';
+export * as JavaScript from './javascript';
+export * as System from './system';
+export * from './utils/languageUtils';
